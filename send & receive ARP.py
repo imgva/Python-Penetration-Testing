7fb0532db3e676067f40b21258a371ce3aa2ca96
JEEVA R <jeevarjey@gmail.com>
2019-07-06 22:28:51 +0530

    Create send & receive ARP.py

---
+++ send & receive ARP.py
@@ -0,0 +1,10 @@
+import scapy.all as scapy
+
+def scan(ip)
+ arp_request=scapy.ARP(pdst==ip)
+ broadcast=scapy.Ether(" ")
+ arp_request_broadcast=broadcast/arp_request
+ answered,unanswered= scapy.srp(arp_request_broadcast,timeout=1)
+ print(answered,summary()
+ 
+scan("10.0.2.1/24")
